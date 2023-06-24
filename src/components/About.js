5ac057ce0054cdfe846090802052fda3bbefb2f1
Izdihar <ezdiharomerrrr@gmail.com>
2023-06-24 16:16:53 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function About() {
+
+
+
+export default function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={process.env.PUBLIC_URL + '/images/demo.png'} alt="I made this" />
     </div>
   );
 }
